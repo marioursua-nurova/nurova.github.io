47af75773f12553da86e7838dd2c26e702fc48be
marioursua-nurova <marioursua@nurova.studio>
2025-08-14 23:00:27 -0700

    Update nurova bg flow.js

--- nurova bg flow.js
+++ nurova bg flow.js
@@ -49,7 +49,7 @@ let config = {
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
     BACK_COLOR: { r: 238, g: 244, b: 244 },
-    TRANSPARENT: false,
+    TRANSPARENT: true,
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
     BLOOM_RESOLUTION: 256,
